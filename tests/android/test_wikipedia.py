7dcd9c9c8219fa170d386da560efe525c97744cf
irina <neeiraaaa@gmail.com>
2023-02-28 17:30:22 +0300

    add hw with Browserstack

--- tests/android/test_wikipedia.py
+++ tests/android/test_wikipedia.py
@@ -1,9 +1,12 @@
+import allure
+from allure import step as title
 from appium.webdriver.common.appiumby import AppiumBy
-from selene import have
+from selene import have, be
 from selene.support.shared import browser
 from allure import step
 
 
+@allure.title("Wikipedia search BrowserStack")
 def test_search():
     with step('Type search'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
@@ -15,3 +18,14 @@ def test_search():
         browser.all(
             (AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')
         ).should(have.size_greater_than(0))
+
+
+@allure.title("Wikipedia search selene and check the content of the phrase")
+def test_search_selene():
+    with title('Type search'):
+        browser.element('Search Wikipedia').tap()
+        browser.element('#search_src_text').type('selene')
+
+    with title('Verify content found'):
+        browser.all('#page_list_item_title').should(have.size_greater_than(0))
+        browser.element('«Ancient Greek goddess of the Moon»').should(be.visible)
